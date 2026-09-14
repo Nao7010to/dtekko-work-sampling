6718e68a6c928fba67bde0723ded5966c9d46786
Nao7010to <fel.a619.a216@icloud.com>
2026-09-14 10:58:37 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,13 @@
-const CACHE="dtekko-ws-v3";
+const CACHE="dtekko-ws-v4";
 const ASSETS=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener("fetch",e=>{
  if(e.request.mode==="navigate"){
-   e.respondWith(fetch(e.request).then(r=>{const cp=r.clone();caches.open(CACHE).then(c=>c.put("./index.html",cp));return r}).catch(()=>caches.match("./index.html")));
- }else e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+   e.respondWith(fetch(e.request,{cache:"no-store"}).then(r=>{
+     const cp=r.clone();caches.open(CACHE).then(c=>c.put("./index.html",cp));return r;
+   }).catch(()=>caches.match("./index.html")));
+ }else{
+   e.respondWith(fetch(e.request,{cache:"no-store"}).catch(()=>caches.match(e.request)));
+ }
 });

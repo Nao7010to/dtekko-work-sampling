9efac30435419955287752cccff72edfb5792419
Nao7010to <fel.a619.a216@icloud.com>
2026-09-15 22:46:29 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="work-sampling-v22";
+const CACHE="work-sampling-v23";
 const ASSETS=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
 
 self.addEventListener("install",event=>{
